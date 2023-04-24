af1d94426584976795fab11622f3e4ca5e34ed84
malus <maluabril.08@gmail.com>
2023-04-24 17:42:13 -0500

    delete an order detail

--- src/components/Orders/OrderForm.jsx
+++ src/components/Orders/OrderForm.jsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import {Box, Button, InputLabel, MenuItem, Select, TextField} from '@material-ui/core';
+import {Link, useHistory, useParams} from 'react-router-dom';
+import {
+    Box,
+    Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
+    MenuItem,
+    Table, TableBody,
+    TableCell, TableContainer,
+    TableHead,
+    TableRow,
+    TextField
+} from '@material-ui/core';
 import { createOrder, getOrderById, updateOrder } from '../../api/orders';
-import {getAllProducts} from "../../api/products";
+import {deleteProduct, getAllProducts} from "../../api/products";
+import {deleteOrderDetail} from "../../api/orderDetails";
 const OrderForm = ({ order: initialOrder }) => {
 
     const statusOptions = ['Pending', 'InProgress', 'Completed'];
-    const [selectedStatus, setSelectedStatus] = useState('');
+    const [selectedOrderDetail, setSelectedOrderDetail] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
     const history = useHistory();
     const { id } = useParams();
     const [order, setOrder] = useState(initialOrder || {
         orderNumber: '',
+        finalPrice: '0',
+        numberOfProducts: '0',
         status: 'Pending',
     });
     const isEdit = id !== undefined;
     const [currentDate, setCurrentDate] = useState('');
-    const [isModalOpen, setIsModalOpen] = useState(false);
     const [products, setProducts] = useState([]);
     useEffect(() => {
         const fetchOrder = async () => {
@@ -38,13 +50,9 @@ const OrderForm = ({ order: initialOrder }) => {
         }));
     }, []);
 
-    const handleStatusChange = (event) => {
-        const { value } = event.target;
-        setSelectedStatus(value);
-        setOrder((prevOrder) => ({
-            ...prevOrder,
-            status: value,
-        }));
+    const handleDeleteButtonClick = (orderDetail) => {
+        setSelectedOrderDetail(orderDetail);
+        setModalOpen(true);
     };
     const handleInputChange = (event) => {
         const { name, value } = event.target;
@@ -66,21 +74,21 @@ const OrderForm = ({ order: initialOrder }) => {
         history.push('/my-orders');
     };
 
-    const handleOpenModal = () => {
-        setIsModalOpen(true);
-        // Fetch the list of products when the modal is opened
-        getAllProducts()
-            .then((data) => setProducts(data))
-            .catch((error) => console.log(error));
-    };
 
     const handleCloseModal = () => {
-        setIsModalOpen(false);
+        setSelectedOrderDetail(null);
+        setModalOpen(false);
     };
 
-    const handleProductAdded = (product) => {
-        console.log(product);
-        setIsModalOpen(false);
+    const handleOrderDetailDeleted = async (deletedOrderDetail) => {
+        await deleteOrderDetail(deletedOrderDetail.id);
+        const remainingOrderDetails = order.orderDetails.filter((p) => p.id !== deletedOrderDetail.id);
+        setOrder((prevOrder) => ({
+            ...prevOrder,
+            numberOfProducts: remainingOrderDetails.length,
+            orderDetails: remainingOrderDetails
+        }));
+        handleCloseModal();
     };
 
     return (
@@ -107,17 +115,6 @@ const OrderForm = ({ order: initialOrder }) => {
                     margin="normal"
                 />
                 <TextField
-                    label="Final Price"
-                    name="finalPrice"
-                    type="number"
-                    value={order.finalPrice}
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                    fullWidth
-                    margin="normal"
-                />
-                <TextField
                     label="Status"
                     name="status"
                     select
@@ -136,11 +133,100 @@ const OrderForm = ({ order: initialOrder }) => {
                         </MenuItem>
                     ))}
                 </TextField>
+                <TableContainer>
+                    <h3> Order's Products</h3>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Product Id</TableCell>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Unit Price</TableCell>
+                                <TableCell>Quantity</TableCell>
+                                <TableCell>Sub Total</TableCell>
+                                <TableCell>Options</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {order.orderDetails&&order.orderDetails.map((orderDetail) => (
+                                <TableRow key={orderDetail.id}>
+                                    <TableCell>{orderDetail.product.id}</TableCell>
+                                    <TableCell>{orderDetail.product.name}</TableCell>
+                                    <TableCell>{orderDetail.product.unitPrice}</TableCell>
+                                    <TableCell>{orderDetail.quantity}</TableCell>
+                                    <TableCell>{orderDetail.product.unitPrice * orderDetail.quantity}</TableCell>
+                                    <TableCell>
+                                        <Button
+                                            component={Link}
+                                            to={`/add-order/${order.id}`}
+                                            variant="contained"
+                                            color="primary"
+                                        >
+                                            Edit
+                                        </Button>
+                                        <Button
+                                            variant="contained"
+                                            color="secondary"
+                                            onClick={() => handleDeleteButtonClick(orderDetail)}
+                                        >
+                                            Delete
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
+                <Button type="submit" variant="contained">
+                    Add Order Detail
+                </Button>
+
+                <TextField
+                    label="Number of Products"
+                    name="numberOfProducts"
+                    type="number"
+                    value={order.numberOfProducts}
+                    InputProps={{
+                        readOnly: true,
+                    }}
+                    fullWidth
+                    margin="normal"
+                />
+
+                <TextField
+                    label="Final Price"
+                    name="finalPrice"
+                    type="number"
+                    value={order.finalPrice}
+                    InputProps={{
+                        readOnly: true,
+                    }}
+                    fullWidth
+                    margin="normal"
+                />
+
 
                 <Button type="submit" variant="contained" color="primary">
                     {isEdit ? 'Save Changes' : 'Add Order'}
                 </Button>
             </Box>
+
+            <Dialog open={modalOpen} onClose={handleCloseModal}>
+                <DialogTitle>{`Delete Order Detail ${selectedOrderDetail?.id}`}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to delete the detail product "{selectedOrderDetail?.product.name}"?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseModal} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={() => handleOrderDetailDeleted(selectedOrderDetail)} color="secondary" autoFocus>
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
 
         </form>
     );
